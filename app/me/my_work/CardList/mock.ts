4d4469f49530fb9cf2e7125f3655e81a350d7b3e
YujiaWangAaron <wyujia@umich.edu>
2024-10-07 22:23:24 -0700

    add me page

--- app/me/my_work/CardList/mock.ts
+++ app/me/my_work/CardList/mock.ts
@@ -16,29 +16,12 @@ import Image from "next/image";
 
 export const dataSource = [
   {
-    title: "element-hyper",
-    img: web1, // Path relative to public folder
-    price: "John Smith",
-    date: "38 mins ago",
-  },
-  {
     title: "business-gator",
     img: web2,
     price: "Emily Davis",
     date: "2 days ago",
   },
-  {
-    title: "kiwi-bigfan",
-    img: web3,
-    price: "Michael Johnson",
-    date: "4 days ago",
-  },
-  {
-    title: "jake chen",
-    img: web4,
-    price: "Sarah Lee",
-    date: "4 days ago",
-  },
+
   {
     title: "Avocado-kill",
     img: web5,
@@ -69,24 +52,7 @@ export const dataSource = [
     price: "David Thomas",
     date: "19 days ago",
   },
-  {
-    title: "JumpJump Tiger",
-    img: web10,
-    price: "Isabella Hernandez",
-    date: "20 days ago",
-  },
-  {
-    title: "Tiger kingdom",
-    img: web11,
-    price: "William Garcia",
-    date: "21 days ago",
-  },
-  {
-    title: "Beijing mayor",
-    img: web12,
-    price: "Mia Robinson",
-    date: "28 days ago",
-  },
+
   {
     title: "road murder",
     img: web13,
